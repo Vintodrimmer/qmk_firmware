14edd1d8d357e02c974ad88cd6b0390004bb486c
Vintodrimmer <vintodrimmer@protonmail.ch>
2018-03-11 10:09:52 +0100

    Now definitions should be correct. Thanks, jmagee.

---
+++ quantum/keymap_extras/keymap_russian.h
@@ -0,0 +1,60 @@
+/* Copyright 2016 Didier Loiseau
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/* Keymap macros for the French BÉPO layout - http://bepo.fr */
+#ifndef KEYMAP_RUSSIAN
+#define KEYMAP_RUSSIAN
+
+#include "keymap.h"
+
+// Normal characters
+
+// All the letters of the alphabet
+#define RU_Q	0xB9 // й
+#define RU_W	0x86 // ц
+#define RU_E	0x83 // у
+#define RU_R	0xBA // к
+#define RU_T	0xB5 // е
+#define RU_Y	0xBD // н
+#define RU_U	0xB3 // г
+#define RU_I	0x88 // ш
+#define RU_O	0x79 // щ
+#define RU_P	0xB7 // з
+#define RU_A	0x84 // ф
+#define RU_S	0x8B // ы
+#define RU_D	0xB2 // в
+#define RU_F	0xB0 // а
+#define RU_G	0xBF // п
+#define RU_H	0x80 // р
+#define RU_J	0xBE // о
+#define RU_K	0xBB // л
+#define RU_L	0xB4 // д
+#define RU_SCLN	0xB6 // ж
+#define RU_QUOT	0x8D // э
+#define RU_Z	0x8F // я
+#define RU_X	0x87 // ч
+#define RU_C	0x81 // с
+#define RU_V	0xBC // м
+#define RU_B	0xB8 // и
+#define RU_N	0x82 // т
+#define RU_M	0x8C // ь
+#define RU_COMM	0xB1 // б
+#define RU_DOT	0x8E // ю
+#define RU_SLSH	0x2E // .
+#define RU_TILD	0x91 // ё
+#define RU_LBRC	0x85 // х
+#define RU_RBRC	0x8A // ъ
+
+#endif
